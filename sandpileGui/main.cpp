0f60b5c4a3e3b0ecf7d4755b47c9254ef39ef6cf
andrewnelsonnyc@gmail.com <ajnels@DESKTOP-BCQ7725>
2023-08-30 23:27:11 -0400

    finished image rescaling

--- sandpileGui/main.cpp
+++ sandpileGui/main.cpp
@@ -1,15 +1,17 @@
 #include <opencv2/core/core.hpp>
-#include "sandpile/sand.cpp"
+#include "../sand.cpp"
 #include <vector>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
 #include <thread>
 #include <chrono>
 
 
 int main() {
 
-  SandpileModel model(720,1280);
-  model.iterate(10000);
+  int factor = 5;
+  SandpileModel model(1080/factor,1920/factor);
+  model.iterate(1);
   while (true){
     // Sample 2D vector
     std::vector<std::vector<int>> data = model.getGrid();
@@ -19,21 +21,27 @@ int main() {
 
     cv::Mat image(rows, cols, CV_8SC1);  // 8-bit signed single channel matrix
 
-    for (int i = 0; i < rows; i += 2) {
-      for (int j = 0; j < cols; j += 2) {
+    for (int i = 0; i < rows; i += 1) {
+      for (int j = 0; j < cols; j += 1) {
 	image.at<int8_t>(i, j) = data[i][j];
-	image.at<int8_t>(i, j+1) = data[i][j];
-	image.at<int8_t>(i+1, j) = data[i][j];
-	image.at<int8_t>(i+1, j+1) = data[i][j];
       }
     }
+    // resize the image
+    cv::Mat scaled_grid;
+    
+    cv::resize(image,
+	       scaled_grid,
+	       cv::Size(), factor, factor,
+	       cv::INTER_NEAREST);
 
-    cv::normalize(image, image, 0, 255, cv::NORM_MINMAX, CV_8UC1);  // Normalize for visualization
-
-    cv::imshow("Grayscale Image", image);
+    cv::normalize(scaled_grid,
+		  scaled_grid,
+		  0, 255,
+		  cv::NORM_MINMAX, CV_8UC1);
+    cv::imshow("Grayscale Image", scaled_grid);
     cv::waitKey(1);
     
-    model.iterate(10);
+    model.iterate(1);
   }
   return 0;
 }
